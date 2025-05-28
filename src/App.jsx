36c4eb172576e80195fe9742be7f46997621aec6
unknown <rum3152@gmail.com>
2025-05-28 20:37:04 +0300

    Slojen e navbar i stranici

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/home'
+import Login from './pages/login'
+import Register from './pages/register'
+import Dashboard from './pages/dashboard'
+import Navbar from './components/navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  return (
+    <><Navbar /><Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes></>
+    
+  )
   
 }
+
+
 
 export default App
